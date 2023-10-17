8e79683fcc6c0f513feb93655c29c05a74cf3fcd
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-10-17 18:15:41 +0600

    current location

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,16 +1,18 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-  static const CameraPosition _kGooglePlex = CameraPosition(
-      target: LatLng(23.727150967403755, 90.38139997468986), zoom: 14);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final CameraPosition _kGooglePlex = const CameraPosition(
+      target: LatLng(23.727150967403755, 90.38139997468986), zoom: 14);
   final Completer<GoogleMapController> _controller = Completer();
 
   final List<Marker> _marker = [];
@@ -24,10 +26,34 @@ class _MyHomePageState extends State<MyHomePage> {
         position: LatLng(20.727150967403755, 70.38139997468986),
         infoWindow: InfoWindow(title: "wohooo!!!"))
   ];
+  loadLocation() {
+    getCurrentLocation().then((value) async {
+      print("my current location: \n${value.latitude} ${value.longitude}");
+      _marker.add(Marker(
+          markerId: const MarkerId("3"),
+          position: LatLng(value.latitude, value.longitude),
+          infoWindow: const InfoWindow(title: "Sifat's current location")));
+
+      CameraPosition cmpstn = CameraPosition(
+          zoom: 14, target: LatLng(value.latitude, value.longitude));
+      GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(cmpstn));
+      setState(() {});
+    });
+  }
+
+  Future getCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) {
+      print("Error: $error");
+    });
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   void initState() {
-    _marker.addAll(listOfMarker);
+    loadLocation();
     super.initState();
   }
 
@@ -38,8 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Google Map'),
       ),
       body: GoogleMap(
-        initialCameraPosition: MyHomePage._kGooglePlex,
-        markers: Set.of(listOfMarker),
+        initialCameraPosition: _kGooglePlex,
+        markers: Set.of(_marker),
         onMapCreated: (controller) {
           _controller.complete(controller);
         },
@@ -48,15 +74,14 @@ class _MyHomePageState extends State<MyHomePage> {
         alignment: Alignment.bottomLeft,
         margin: const EdgeInsets.only(left: 30, bottom: 20),
         child: FloatingActionButton(
-            child: Icon(Icons.location_history),
-            onPressed: () async {
-              GoogleMapController controller = await _controller.future;
-              controller.animateCamera(CameraUpdate.newCameraPosition(
-                CameraPosition(
-                    target: LatLng(20.727150967403755, 70.38139997468986),
-                    zoom: 14),
-              ));
-              setState(() {});
+            child: const Icon(Icons.location_history),
+            onPressed: () {
+              // GoogleMapController controller = await _controller.future;
+              // controller.animateCamera(CameraUpdate.newCameraPosition(
+              //   const CameraPosition(
+              //       target: LatLng(20.727150967403755, 90.381399977), zoom: 14),
+              // ));
+              // setState(() {});
             }),
       ),
     );
